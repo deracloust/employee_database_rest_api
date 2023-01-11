d647bee12d930a03f6ecc19bf1e7a2d46d0cfb19
deracloust <michal.koloodynski@gmail.com>
2023-01-11 20:03:31 +0100

    added error status to authentication fail response

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,18 +3,18 @@ const jwt = require('jsonwebtoken')
 module.exports = (req, res, next) => {
 	const authHeader = req.get('Authorization')
 	if (!authHeader) {
-		return res.status(401).json({ message: `Not authenticated!` })
+		return res.status(401).json({ errorStatus: 401, message: `Not authenticated!` })
 	}
 	let decodedToken
 
 	try {
 		decodedToken = jwt.verify(authHeader, 'SuperSecret!')
 	} catch (err) {
-		return res.status(401).json({ message: `Not authenticated!` })
+		return res.status(401).json({ errorStatus: 401, message: `Not authenticated!` })
 	}
 
 	if (!decodedToken) {
-		return res.status(401).json({ message: `Not authenticated!` })
+		return res.status(401).json({ errorStatus: 401, message: `Not authenticated!` })
 	}
 	req.userId = decodedToken.userId
 	next()
